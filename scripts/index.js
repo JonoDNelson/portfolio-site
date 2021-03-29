59bb7799b52f5f4e1e7b3db0ab9f60d485d3376e
Jono Nelson <jonodnelson@gmail.com>
2021-03-28 21:31:13 -0400

    Cleaned up some responsiveness, added nav section highlights

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,49 @@
 /*
    TODO: 
    * Throttle onScroll
-   * Highlight correct button according to section in onScroll
+   * CONTACT FORM
 */
 /*======== Static Variables ==========*/
 // For sticky nav
 var navbar = document.getElementById("navbar");
+document.getelement
 var stickyNavLoc = navbar.offsetTop;
+// For highlighting nav links
+let navLinks = document.querySelectorAll("nav ul li a");
+let sections = Array.from(document.getElementsByClassName("section"));
+
 // For slides navigation
 var slideIndex = 0;
 var slides = Array.from(document.getElementsByClassName("slide"));
 var dots = Array.from(document.getElementsByClassName("dot"));
 
 /*======== onScroll Functionality ==========*/
-window.onscroll = function() {onScroll()};
+window.onscroll = function() {
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function onScroll() {
+  // For sticky nav
+  // Add sticky class to the navbar when you reach its scroll position.
+  // Remove "sticky" when you leave the scroll position
   if (window.pageYOffset >= stickyNavLoc) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
+  }
+
+  // For highlighting nav links
+  let fromTop = window.scrollY;
+  let el = document.querySelector('nav .active')
+  if(el) {
+    el.classList.remove('active');
+  }
+  
+  if(fromTop < sections[1].offsetTop) {
+    navLinks[0].classList.add('active');
+  } else if (fromTop < sections[2].offsetTop) {
+    navLinks[1].classList.add('active');
+  } else if (fromTop < sections[3].offsetTop) {
+    navLinks[2].classList.add('active');
+  } else {
+      navLinks[3].classList.add('active');
   }
 }
 
